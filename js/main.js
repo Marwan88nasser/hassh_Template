49c4e1bafa8fcaca94aefdbb765cf9678a5ffafe
unknown <marwan.nasser565@gamil.com>
2023-04-08 15:34:32 +0200

    last update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+var swiper = new Swiper(".testimonial-swiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
 // make modal Bootstrap popup show without click
 if (document.querySelector("#popup_show")) {
   let myModal = new bootstrap.Modal(document.getElementById("popup_show"));
